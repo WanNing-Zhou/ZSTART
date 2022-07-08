ee85bc47aecb5329f07e7073c5d3a08e95b99a1e
WanNing-Zhou <1879154660@qq.com>
2022-07-08 17:00:25 +0800

    update

--- z/static/app.js
+++ z/static/app.js
@@ -37,6 +37,26 @@ window.onload = function () {
     let input = document.querySelector('input');
     let submit = document.querySelector('.submit');
 
+
+
+    // 定时器，每隔 1 秒执行 1 次
+    setInterval(() => {
+        var dt = new Date()
+        var HH = dt.getHours()
+        var mm = dt.getMinutes()
+        var ss = dt.getSeconds()
+
+        // 为页面上的元素赋值
+        document.querySelector('#HH').innerHTML = padZero(HH)
+        document.querySelector('#mm').innerHTML = padZero(mm)
+        // document.querySelector('#ss').innerHTML = padZero(ss)
+    }, 1000);
+
+    // 补零函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
     body.addEventListener('click', () => {
         selectSearchHidden();
     })
